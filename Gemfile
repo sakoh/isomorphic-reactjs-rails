117d683f9ea356767218698e3e3348d9c64fcf97
sakoh <sakohartounian@yahoo.com>
2015-05-26 13:54:56 -0700

    Guard Rspec added

--- Gemfile
+++ Gemfile
@@ -46,5 +46,7 @@ group :development, :test do
   gem 'spring'
 
   gem 'rspec-rails', '~> 3.0'
+
+  gem 'guard-rspec', require: false
 end
 
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe PostsController, type: :controller do
   
   describe "GET index" do
 
+  	it "gets posts" do
+  	  post = Post.create
+      get :index
+      expect(assigns(:posts)).to eq([post])
+  	end
+
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
